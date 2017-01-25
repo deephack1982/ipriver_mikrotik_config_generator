16cbece3048de81702272d9f764a55482a55754b
Mark Dickie <mark.dickie@gmail.com>
2017-01-25 18:52:38 +0000

    Started work on detecting interface type and branching ConfigGenerator based on this.

--- app/controllers/concerns/config_generator.rb
+++ app/controllers/concerns/config_generator.rb
@@ -4,10 +4,12 @@ module ConfigGenerator
     @output = "/system identity set name=#{@config.system_name}\n"
     @output << "/user set admin password=#{@config.password}\n"
     @output << @config.config_template.config_text + "\n"
-    @output << "/ip address add address=#{@config.interfaces.first.ip}/#{@config.interfaces.first.subnet} comment=WAN interface=ether1\n"
-    @output << "/ip route add dst-address=0.0.0.0 gateway=#{@config.interfaces.first.gateway}\n"
-    @output << "/ip address add address=#{@config.interfaces.second.ip}/#{@config.interfaces.second.subnet} comment=LAN interface=ether2-master\n"
-    @output << "/ip dns static add address=#{@config.interfaces.second.ip} name=router\n"
+    if @config.interfaces.first.interface_type == 'ethernet'
+      @output << "/ip address add address=#{@config.interfaces.first.ip}/#{@config.interfaces.first.subnet} comment=WAN interface=ether1\n"
+      @output << "/ip route add dst-address=0.0.0.0 gateway=#{@config.interfaces.first.gateway}\n"
+      @output << "/ip address add address=#{@config.interfaces.second.ip}/#{@config.interfaces.second.subnet} comment=LAN interface=ether2-master\n"
+      @output << "/ip dns static add address=#{@config.interfaces.second.ip} name=router\n"
+    end
     return @output
   end
 end
